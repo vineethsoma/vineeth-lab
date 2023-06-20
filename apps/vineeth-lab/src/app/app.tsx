827924a49f0d953da797ef4be3f7708dafed0853
vineeth soma <vineethsoma@vineeths-MacBook-Pro.local>
2023-06-20 14:45:16 -0400

    Initial commit

---
+++ apps/vineeth-lab/src/app/app.tsx
@@ -0,0 +1,17 @@
+import styled from 'styled-components';
+
+import NxWelcome from './nx-welcome';
+
+const StyledApp = styled.div`
+  // Your style here
+`;
+
+export function App() {
+  return (
+    <StyledApp>
+      <NxWelcome title="vineeth-lab" />
+    </StyledApp>
+  );
+}
+
+export default App;
